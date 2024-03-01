7118948914b2439bc4c030eb261e4e8d0f5f242b
Dantas <dantas10.crispim@gmail.com>
2024-02-29 22:32:27 -0300

    add mais um acondição à lógica

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 
 function calcular() {
-    
+
     const input1 = document.getElementById("one").value;
     const input2 = document.getElementById("two").value;
     const res = document.getElementById("res");
 
     const result = Number(input1 / input2);
-
-    if(result < 0.7) {
+     
+    if(result <= 0.7 ) {
         res.innerHTML = `Vale mais a pena usar Álcool!`
     }else {
         res.innerHTML = `Vale mais a pena usar Gasolina!`
     }
-    
+     if( !result) { 
+       res.innerHTML = `Por favor informa valores válidos para a conta.`
+     }
 }
